80efa796a6167daf5c493210f5b544a7b91ade07
aneeshsingh <aneesh@alkye.com>
2023-03-11 09:51:40 +0530

    landing

--- law-website/js/custom.js
+++ law-website/js/custom.js
@@ -21,4 +21,30 @@ $(document).ready(function(){
             }
         }
     })
+
+    $('.owl-partner').owlCarousel({
+        loop:true,
+        margin:15,
+        nav:false,
+        dots: true,
+        // navText: ['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:3
+            },
+            480:{
+                items:4
+            },
+            768:{
+                items:5
+            },
+            1000:{
+                items:6
+            },
+            1280:{
+                items:7
+            }
+        }
+    })
 })
